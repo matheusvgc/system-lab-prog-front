48dcd3a658bde28287e62c2eb5a168b2d539216c
MarkiNzera <vstar516@gmail.com>
2025-02-12 21:51:43 -0300

    feat: home page

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -2,6 +2,79 @@
 
 export default function HomePage() {
     return (
-        <p>Home asdas</p>
+        <div className="bg-gray-100 pb-10">
+            <h1 className="text-center text-4xl p-10">Nossos produtos</h1>
+            <ul className="flex flex-row justify-center gap-10">
+                <li>Categorias</li>
+                <li>Marca</li>
+                <li>Ordenar por</li>
+            </ul>
+            <div className="w-7xl mx-auto my-10 p-10 flex flex-wrap gap-10 border-black-500 border-2">
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                <div>
+                    <img src="../../public/product image.png" alt="Imagem do produto" />
+                    <h2>Nome do produto</h2>
+                    <p>Descrição do produto</p>
+                    <p>Preço: R$ 99,99</p>
+                    <button>Comprar</button>
+                </div>
+                
+            </div>
+        </div>
     )
 }
