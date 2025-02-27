9622428ea6889455532d42d42f8c3acb10b9df26
Marcos Oliveira <95980171+MarcosVDSO@users.noreply.github.com>
2025-02-27 00:59:39 -0300

    style: adjustment cart page

--- src/components/ui/BaseButton.tsx
+++ src/components/ui/BaseButton.tsx
@@ -1,14 +1,32 @@
+import clsx from "clsx";
 
 interface ButtonProps {
     children?: React.ReactNode;
     onClick?: () => void;
+    bgColor?: string;
+    textColor?: string;
+    hoverColor?: string;
 }
 
-export default function BaseButton({ children, onClick }: ButtonProps) {
+export default function BaseButton({
+    children,
+    onClick,
+    bgColor = "bg-gray-600",
+    textColor = "text-white",
+    hoverColor = "hover:bg-gray-700",
+}: ButtonProps) {
     return (
-        <button type="button" onClick={onClick} className="bg-gradient-to-r from-gray-600 to-gray-800 hover:from-gray-700 hover:to-gray-900 text-white font-bold py-2 px-4 rounded cursor-pointer text-sm">
+        <button
+            type="button"
+            onClick={onClick}
+            className={clsx(
+                bgColor,
+                hoverColor,
+                textColor,
+                "font-bold py-2 px-4 rounded cursor-pointer text-sm transition duration-300"
+            )}
+        >
             {children}
         </button>
-    )
+    );
 }
-
--- src/pages/cartPage.tsx
+++ src/pages/cartPage.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/header";
 
 import phoneImage from "../../public/product image.png";
 import { useState } from "react";
+import BaseButton from "@/components/ui/BaseButton";
 
 const _products = [
     {
@@ -53,53 +54,53 @@ export default function CartPage() {
 
     return (
         <>
-            <Header/>
-            <h1 className="text-4xl text-black text-center m-5">Seu carrinho</h1>
+            <Header />
+            <h1 className="text-xl text-black text-center m-5">Seu carrinho</h1>
             <div className="flex justify-center m-5 gap-2">
                 <div>
                     <table className="w-4xl overflow-y-auto min-h-96 max-h-96">
                         <thead>
                             <tr>
-                                <th></th>
+                                <th>{null}</th>
                                 <th>Produto</th>
                                 <th>Quantidade</th>
                                 <th>Preço</th>
-                                <th></th>
+                                <th>{null}</th>
                             </tr>
                         </thead>
                         <tbody className="">
                             {products.map(product => (
                                 <tr key={product.productId}>
-                                    <td><img src={product.image} alt="Fone de ouvido" className="w-20 h-20"/></td>
+                                    <td><img src={product.image} alt="Fone de ouvido" className="w-20 h-20" /></td>
                                     <td className="text-center">{product.productName}</td>
                                     <td className="text-center">
-                                        <button className="p-2">-</button>99<button className="p-2">+</button>
+                                        <button type="button" className="p-2">-</button>99<button type="button" className="p-2">+</button>
                                     </td>
                                     <td className="text-center">R$ {product.price}</td>
-                                    <td className="text-center"><button onClick={() => handleDelete(product.productId)} className="bg-red-400 rounded-lg p-2 cursor-pointer">Remover</button></td>
+                                    <td className="text-center"><BaseButton bgColor="bg-red-700" hoverColor="hover:bg-red-800" onClick={() => handleDelete(product.productId)} >Remover</BaseButton></td>
                                 </tr>
                             ))}
                         </tbody>
                     </table>
-                    <div className="px-5 text-right text-2xl">
-                        Subtotal: R$ 203,63
+                    <div className="text-right text-lg pr-12">
+                        {'Subtotal (6 produtos): R$ 203,63'}
                     </div>
                 </div>
                 <div>
-                    <div className="bg-gray-200 p-5 rounded-lg">
+                    <div className="p-5 rounded-lg flex flex-col gap-4">
                         <div className="flex flex-col gap-2">
                             <p>Subtotal: R$ 203,63</p>
                             <p>Descontos: R$ -2,87</p>
                             <p>Entrega: R$ 0,00</p>
                             <p>Total: R$ 203,63</p>
                         </div>
-                        <div className="flex justify-center w-xs my-2">
-                            <button className="bg-green-400 rounded-lg p-2 cursor-pointer">Finalizar pedido</button>
+                        <div className="flex w-xs my-2">
+                            <BaseButton bgColor="bg-green-600" hoverColor="hover:bg-green-700">Finalizar Pedido</BaseButton>
                         </div>
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
