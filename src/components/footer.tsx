14bd7317866a87299c6b400bd0b44c397ead9b80
MarkiNzera <vstar516@gmail.com>
2025-03-10 17:34:30 -0300

    feat: responsive footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,33 @@
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col justify-center gap-20 bg-black text-white p-8 text-center text-xl">
-            <div>
+        <footer className="bg-black text-white p-8 text-center text-xl">
+            <div className="my-4">
                 TECH SHOP &copy; 2022
             </div>
-            <div>
-                Contatos
-                <ul>
-                    <li>(98) 99999-9999</li>
-                    <li>(98) 99999-9999</li>
-                </ul>
-            </div>
-            <div>
-                Redes sociais
-                <ul>
-                    <li>Instagram: @Instagram</li>
-                    <li>Facebook: @Facebook</li>
-                </ul>
-            </div>
-            <div>
-                Formas de pagamento
-                <ul>
-                    <li>Cartão de crédito</li>
-                    <li>Cartão de débito</li>
-                    <li>Boleto</li>
-                </ul>
+            <div className="md:flex md:gap-4 md:justify-center">
+                <div className="my-4">
+                    Contatos
+                    <ul>
+                        <li>(98) 99999-9999</li>
+                        <li>(98) 99999-9999</li>
+                    </ul>
+                </div>
+                <div className="my-4">
+                    Redes sociais
+                    <ul>
+                        <li>Instagram: @Instagram</li>
+                        <li>Facebook: @Facebook</li>
+                    </ul>
+                </div>
+                <div className="my-4">
+                    Formas de pagamento
+                    <ul>
+                        <li>Cartão de crédito</li>
+                        <li>Cartão de débito</li>
+                        <li>Boleto</li>
+                    </ul>
+                </div>
             </div>
         </footer>
     )
