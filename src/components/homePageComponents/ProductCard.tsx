0fc6b203688422b105cde6f2682fe6f338e38414
MarkiNzera <vstar516@gmail.com>
2025-03-10 16:57:47 -0300

    style: fixed height to content of product card

--- src/components/homePageComponents/ProductCard.tsx
+++ src/components/homePageComponents/ProductCard.tsx
@@ -9,9 +9,11 @@ export default function ProductCard({product}: Props) {
     return (
         <div className="justify-self-center max-w-[250px] flex flex-col gap-2 p-4 cursor-pointer rounded-lg border-1 hover:bg-gray-300 text-center">
             <img className="w-[200px] h-[200px] rounded-lg mx-auto" src="../../product image.png" alt="Imagem do produto" />
-            <h2 className="text-xl">{product.product?.productName || "undefined"}</h2>
-            <p>{product.product?.summary || "undefined"}</p>
-            <p>Preço: R$ {product.price || "undefined"}</p>
+            <h2 className="text-xl min-h-2">{product.product?.productName || "undefined"}</h2>
+            <div className="flex flex-col justify-around min-h-22">
+                <p>{product.product?.summary || "undefined"}</p>
+                <p>Preço: R$ {(product.price / 100).toFixed(2) || "undefined"}</p>
+            </div>
             <button className="mx-auto px-4 py-2 bg-gray-500 rounded-lg text-white cursor-pointer hover:bg-gray-400 hover:text-gray-100">Adicionar ao carrinho</button>
         </div>
     )
