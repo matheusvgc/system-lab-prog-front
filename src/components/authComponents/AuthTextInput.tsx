1a938299ef95704d3fce4a8b0b37918d4430c40b
MarkiNzera <vstar516@gmail.com>
2025-03-10 17:28:51 -0300

    feat: sign up page using customer register api route

--- src/components/authComponents/AuthTextInput.tsx
+++ src/components/authComponents/AuthTextInput.tsx
@@ -1,17 +1,19 @@
 
 interface Props {
     label: string;
+    name: string;
     placeholder: string;
     type: string;
     handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function AuthTextInput({ label, placeholder, type, handleInputChange }: Props) {
+export default function AuthTextInput({ label, name, placeholder, type, handleInputChange }: Props) {
     return (
         <div className="flex flex-col">
             <label>{label}</label>
             <input 
                 className="bg-gray-200 p-2 border-2 rounded-lg shadow-xl" 
+                name={name}
                 type={type}
                 placeholder={placeholder}
                 onChange={handleInputChange}
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -2,14 +2,14 @@ import LoginHeader from "@/components/loginHeader"
 import AuthTextInput from "@/components/authComponents/AuthTextInput";
 import { useState } from "react"
 import Footer from "@/components/footer";
+import api from "@/services/api";
 
 interface IUser {
-    name: string;
-    lastName: string;
+    firstname: string;
+    lastname: string;
     cpf: string;
-    birthDate: string;
     email: string;
-    phone: string;
+    username: string;
     password: string;
     confirmPassword: string;
 }
@@ -17,12 +17,11 @@ interface IUser {
 export default function SignUpPage() {
 
     const [newUser, setNewUser] = useState<IUser>({
-        name: "",
-        lastName: "",
+        firstname: "",
+        lastname: "",
         cpf: "",
-        birthDate: "",
         email: "",
-        phone: "",
+        username: "",
         password: "",
         confirmPassword: "",
     });
@@ -31,10 +30,17 @@ export default function SignUpPage() {
         setNewUser({...newUser, [e.target.name]: e.target.value });
     }
 
-    function submitForm(e: React.FormEvent<HTMLFormElement>) {
+    async function submitForm(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         if (newUser.password !== newUser.confirmPassword) return;
-        console.log(newUser);
+        
+        try {
+            const newCustomer = await api.post("/auth/customer/register", newUser);
+            console.log(newCustomer.data);
+        } catch (err) {
+            console.error(err);
+        }
+
     }
 
     return (
@@ -42,17 +48,16 @@ export default function SignUpPage() {
             <LoginHeader/>
             <form className="p-8 gap-2 mb-5" onSubmit={submitForm}>
                 <h1 className="text-center text-2xl mb-5">Cadastro</h1>
-                <div className="grid grid-cols-2 gap-4 mb-5">
-                    <AuthTextInput label="Nome" placeholder="Digite seu nome" type="text" handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="Sobrenome" placeholder="Digite seu sobrenome" type="text"  handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="CPF" placeholder="Digite seu CPF" type="text"  handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="Data de nascimento" placeholder="Digite sua data de nascimento" type="text"  handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="Email" placeholder="Digite seu email" type="email"  handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="Telefone" placeholder="Digite seu telefone" type="phone"  handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="Senha" placeholder="Digite sua senha" type="password"  handleInputChange={handleInputChange}/>
-                    <AuthTextInput label="Confirmar senha" placeholder="Confirme a senha" type="password"  handleInputChange={handleInputChange}/>
+                <div className="flex flex-col gap-4 mb-5 md:grid md:grid-cols-2">
+                    <AuthTextInput label="Nome" placeholder="Digite seu nome" type="text" name="firstname" handleInputChange={handleInputChange}/>
+                    <AuthTextInput label="Sobrenome" placeholder="Digite seu sobrenome" type="text" name="lastname" handleInputChange={handleInputChange}/>
+                    <AuthTextInput label="CPF" placeholder="Digite seu CPF" type="text" name="cpf" handleInputChange={handleInputChange}/>
+                    <AuthTextInput label="Email" placeholder="Digite seu email" type="email" name="email"  handleInputChange={handleInputChange}/>
+                    <AuthTextInput label="Senha" placeholder="Digite sua senha" type="password" name="password"  handleInputChange={handleInputChange}/>
+                    <AuthTextInput label="Confirmar senha" placeholder="Confirme a senha" type="password" name="confirmPassword"  handleInputChange={handleInputChange}/>
+                    <AuthTextInput label="Nome de usuário" placeholder="Digite seu nome de usuário" type="text" name="username"  handleInputChange={handleInputChange}/>
                 </div>
-                <div className="mx-auto my-5 py-5 w-200 bg-gray-500 text-center rounded-lg mb-5">
+                <div className="mx-auto my-5 py-5 w-full bg-gray-500 text-center text-white rounded-lg mb-5 hover:cursor-pointer lg:w-200">
                     <button type="submit">Confirmar cadastro</button>
                 </div>
             </form>
